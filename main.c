fd1759582ef266baa003bdd4bd31f6e5f5c913d3
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-04-30 17:05:06 +0300

    Add moving player head

--- main.c
+++ main.c
@@ -1,15 +1,59 @@
-#include <stdio.h>
 #include <conio.h>
+#include <stdio.h>
+
+#define clear() printf("\033[H\033[J")
+#define gotoxy(x, y) printf("\033[%d;%dH", (y), (x))
+
+int getch_noblock() {
+  if (_kbhit())
+    return _getch();
+  else
+    return -1;
+}
+
+enum Direction { RIGHT, LEFT, DOWN, UP };
 
 int main() {
   int lastChar = 0;
+  int width = 30, height = 10;
+  char map[width * height + 1];
+
+  clear();
+
+  int px = 0, py = 0;
+
   while (lastChar != 3) { // Escape when user presses ctrl+c
-    lastChar = _getch();
-    printf("%d\n", lastChar - 'a' + 1);
+    // Handle player movement
+    lastChar = getch_noblock();
+    if (lastChar == 224) { // Arrow key was used
+      lastChar = getch_noblock();
+      if (lastChar == 75) { // Left
+        px -= 1;
+      } else if (lastChar == 77) { // Right
+        px += 1;
+      } else if (lastChar == 72) { // Up
+        py -= 1;
+      } else if (lastChar == 80) { // Down
+        py += 1;
+      }
+    }
+
+    // Render map
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width - 1; x++) {
+        if (px == x && py == y) {
+          map[y * width + x] = '#';
+        } else if (x == 0 || x == width - 2 || y == 0 || y == height - 1) {
+          map[y * width + x] = '0';
+        } else {
+          map[y * width + x] = '.';
+        }
+      }
+      map[y * width + width - 1] = '\n';
+    }
+    map[height * width] = '\0';
+
+    // Move caret to top and print everything at once
+    printf("\e[H%s", map);
   }
-
-  // strcmp("12", "21") == 1 -1 = 0;
-
-  printf("Hello world");
 }
-
