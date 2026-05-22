f69b7f3c15abb363e352b12ac7495017a299640e
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-05-22 16:54:16 +0300

    feat: ✨ Add apples

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -14,6 +16,11 @@ int getch_noblock() {
     return _getch();
   else
     return -1;
+}
+
+float randomFloat() {
+  float r = (float)rand() / (float)RAND_MAX;
+  return r;
 }
 
 enum Direction { RIGHT, LEFT, DOWN, UP };
@@ -45,15 +52,20 @@ int wrap(int a, int b, int c) {
 
 int main() {
   int lastChar = 0;
-  int width = 30, height = 10;
-  int px = 2, py = 2;
+  int width = 30, height = 15;
+  int px = 0, py = 0;
+  int ax = (width - 2) / 2, ay = (height - 2) / 2;
+  int maxScore = (width - 2) * (height - 2);
   int length = width * height;
   int size = 1;
 
+  srand(time(NULL));
   system("chcp 65001");
   printf("\e[A");
 
   drawEmptyMap(width, height);
+
+  printf("\e[%dA\e[%dC@\e[%dD\e[%dB", height - ay - 1, ax + 1, ax + 2, height - ay - 1);
 
   int snake[length];
   memset(snake, py * width + px, sizeof snake);
@@ -70,9 +82,23 @@ int main() {
     else if (lastChar == 115 || lastChar == 224 + 80) py = wrap(py, height, 1); // S or Down arrow
     else continue;
 
-    if (head - tail < 8 && head - tail >= 0) {
+    if (px == ax && py == ay) {
       add(snake, py * width + px, length, &head, &tail);
       size++;
+      int apos = 0;
+      int failed = 0;
+
+      if (size < maxScore) {
+        appleSearch:
+        failed++;
+        ax = randomFloat() * (width - 3);
+        ay = randomFloat() * (height - 3);
+        apos = ay * width + ax;
+        for (int j = 0; j <= size; j++) {
+          if (snake[(tail + j) % length] == apos) goto appleSearch;
+        }
+      } else goto restart;
+
     } else {
       set(snake, py * width + px, length, &head, &tail);
     }
@@ -81,6 +107,11 @@ int main() {
     int x1 = snake[(head + (length - 1)) % length] - y1 * width;
     int y2 = snake[(head + (length - 2)) % length] / width;
     int x2 = snake[(head + (length - 2)) % length] - y2 * width;
+
+    int ty = snake[(tail + (length - 1)) % length] / width;
+    int tx = snake[(tail + (length - 1)) % length] - ty * width;
+    // Remove tail
+    printf("\e[%dA\e[%dC \e[%dD\e[%dB", height - ty - 1, tx + 1, tx + 2, height - ty - 1);
 
     // Move top, right print, left + 1, down
     // Render head
@@ -97,54 +128,58 @@ int main() {
       printf("\e[%dA\e[%dC╺\e[%dD\e[%dB", height - py - 1, px + 1, px + 2, height - py - 1);
     }
 
-    // Render curve behind head
-    if ((y1 == wrap(py, height, -1) && x2 == wrap(px, width, -1)) || (y2 == wrap(py, height, 1) && x1 == wrap(px, width, 1))) {
-      printf("\e[%dA\e[%dC╮\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
-    }
-    else if ((y1 == wrap(py, height, 1) && x2 == wrap(px, width, 1)) || (y2 == wrap(py, height, -1) && x1 == wrap(px, width, -1))) {
-      printf("\e[%dA\e[%dC╰\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
-    }
-    else if ((y1 == wrap(py, height, -1) && x2 == wrap(px, width, 1)) || (y2 == wrap(py, height, 1) && x1 == wrap(px, width, -1))) {
-      printf("\e[%dA\e[%dC╭\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
-    }
-    else if ((y1 == wrap(py, height, 1) && x2 == wrap(px, width, -1)) || (y2 == wrap(py, height, -1) && x1 == wrap(px, width, 1))) {
-      printf("\e[%dA\e[%dC╯\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
-    }
-    else if (y1 == py) {
-      printf("\e[%dA\e[%dC─\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
-    }
-    else if (x1 == px) {
-      printf("\e[%dA\e[%dC│\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+    if (size > 1) {
+      // Render curve behind head
+      if ((y1 == wrap(py, height, -1) && x2 == wrap(px, width, -1)) || (y2 == wrap(py, height, 1) && x1 == wrap(px, width, 1))) {
+        printf("\e[%dA\e[%dC╮\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
+      else if ((y1 == wrap(py, height, 1) && x2 == wrap(px, width, 1)) || (y2 == wrap(py, height, -1) && x1 == wrap(px, width, -1))) {
+        printf("\e[%dA\e[%dC╰\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
+      else if ((y1 == wrap(py, height, -1) && x2 == wrap(px, width, 1)) || (y2 == wrap(py, height, 1) && x1 == wrap(px, width, -1))) {
+        printf("\e[%dA\e[%dC╭\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
+      else if ((y1 == wrap(py, height, 1) && x2 == wrap(px, width, -1)) || (y2 == wrap(py, height, -1) && x1 == wrap(px, width, 1))) {
+        printf("\e[%dA\e[%dC╯\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
+      else if (y1 == py) {
+        printf("\e[%dA\e[%dC─\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
+      else if (x1 == px) {
+        printf("\e[%dA\e[%dC│\e[%dD\e[%dB", height - y1 - 1, x1 + 1, x1 + 2, height - y1 - 1);
+      }
     }
 
-    int ty = snake[(tail + (length - 1)) % length] / width;
-    int tx = snake[(tail + (length - 1)) % length] - ty * width;
-    // Remove tail
-    printf("\e[%dA\e[%dC \e[%dD\e[%dB", height - ty - 1, tx + 1, tx + 2, height - ty - 1);
+    // Render apple
+    printf("\e[%dA\e[%dC@\e[%dD\e[%dB", height - ay - 1, ax + 1, ax + 2, height - ay - 1);
 
     int pos = py * width + px;
     for (int j = 0; j < size - 1; j++) {
       // Reset game
       if (snake[(tail + j) % length] == pos) {
-        printf("Game has ended with a score of: %d.\nPress any key to continue.\n", size);
-        size = 1;
-        py = 0;
-        px = 0;
-        head = 0;
-        tail = 0;
-        memset(snake, py * width + px, sizeof snake);
-        lastChar = _getch();
-
-        printf(
-          "\e[A                                            \n"
-          "\e[2A                                           \n"
-          "\e[%dA",
-          height + 1
-        );
-        drawEmptyMap(width, height);
-        break;
+        goto restart;
       }
     }
+
+    continue;
+
+    restart: 
+    printf("Game has ended with a score of: %d.\nPress any key to continue.\n", size);
+    size = 1;
+    py = 0;
+    px = 0;
+    head = 0;
+    tail = 0;
+    memset(snake, py * width + px, sizeof snake);
+    lastChar = _getch();
+
+    printf(
+      "\e[A                                            \n"
+      "\e[2A                                           \n"
+      "\e[%dA",
+      height + 1
+    );
+    drawEmptyMap(width, height);
   }
 
   printf("Exited game with a score of: %d", size);
